aa2034d0df1f0f3073f445a99c1b4f79b935bdf1
ariefpss <permanaarief8@gmail.com>
2023-01-19 15:35:16 +0900

    modified:   app.js 	modified:   package.json

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ const userRouter = require('./API/router/user');
 app.use(bodyPraser.urlencoded({extended: false}));
 app.use(bodyPraser.json());
 
-mongoose.connect(
-    'mongodb://localhost:27017/restapi',
-    {useNewUrlParser: true, useCreateIndex: true},
-    ()=>{
-        console.log("berhasil connect database");
-    }
-);
+mongoose
+    .connect('mongodb://127.0.0.1:27017/restapi',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connect to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+mongoose.set('strictQuery', false);
 
 app.use('/user', userRouter);
 
